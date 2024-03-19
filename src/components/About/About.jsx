310430e9bf05439e23f49b22b5e66304327164cb
Kamalesh Waran <kwaranwork98@gmail.com>
2024-03-19 22:39:22 +0800

    About Page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className="dark:bg-black dark:text-white duration-300 sm:min-h-[600px] sm:grid sm:place-items-center">
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
-          <div data-aos="slide-right" data-aos-duration="1000">
+          <div data-aos="slide-right" data-aos-duration="1500">
             <img
               src={Car2}
               alt=""
